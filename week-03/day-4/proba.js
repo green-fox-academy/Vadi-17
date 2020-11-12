be42808de0ff77ab2a9ca3ec35ccba5e73e887de
Vadas István <vadas.istvan.1@gmail.com>
2020-11-12 18:12:25 +0100

    proa

---
+++ week-03/day-4/proba.js
@@ -0,0 +1,27 @@
+'use strict';
+const lineCount = 4;
+const halfLineCount = Math.floor(lineCount/2);
+let result;
+for ( let i=lineCount-1; i>=0; i--){
+    result = "";
+    for (let j=0; j<lineCount; j++){
+        if( (i+j) < halfLineCount || (i+j) >=lineCount+halfLineCount || Math.abs(i-j) > halfLineCount ){
+            result = result + " ";
+        } else {
+            result = result + "*";
+        }
+    }
+    console.log(result);
+}
+
+for ( let i=lineCount; i>=1; i--){
+    result = "";
+    for (let j=1; j<=lineCount; j++){
+        if( i == lineCount || i == 1 || j == lineCount || j == 1 || i+j == lineCount+1 ) {
+            result = result + "%";
+        } else {
+            result = result + " ";
+        }
+    }
+    console.log(result);
+}
